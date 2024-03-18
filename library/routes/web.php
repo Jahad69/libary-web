ae0bf3a349b106b54188ef7cd9cbeb1856bb53de
Lucas <feelingsleepy4@gmail.com>
2024-03-18 16:41:09 +0700

    cadt

--- library/routes/web.php
+++ library/routes/web.php
@@ -16,13 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('/test', function () {
-    return view('welcome');
-});
-<<<<<<< HEAD
-
-
-=======
->>>>>>> da4bbc37372009fda6125399776f6c8e7caa1177
